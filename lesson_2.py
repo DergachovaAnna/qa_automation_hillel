88840af47b1ea01f499610df94575acfed93f2e7
DergachovaAnna <ann.dergachova@gmail.com>
2023-01-18 23:19:15 +0100

    done task 3 lesson 2

--- lesson_2.py
+++ lesson_2.py
@@ -7,3 +7,14 @@ eleks_personnel = ["Margo", "Andrey", "Billy", "James", "Ann"]
 new_company_list = toshiba_personnel.copy()
 new_company_list.extend(eleks_personnel)
 print(new_company_list)
+
+# You have 3 groups of people casino_blacklist, poker_blacklist, alcohol_blacklist.
+# Names of people in the format "John Dow" first and second name. Find those who are on all blacklists.
+
+casino_blacklist = {"Han Solo", "Master Yoda", "Obi Wan Kenobi", "R2-D2", "Luke Skywalker"}
+poker_blacklist = {"Graf Dooku", "Leia Organa", "Obi Wan Kenobi", "Luke Skywalker"}
+alcohol_blacklist = {"Master Yoda", "Luke Skywalker", "Leia Organa", "Obi Wan Kenobi"}
+
+
+total_blacklist = casino_blacklist.intersection(poker_blacklist, alcohol_blacklist)
+print(total_blacklist)
