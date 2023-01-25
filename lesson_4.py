ac7a44a1189bc123ae4c8f65d5d0addbca585412
DergachovaAnna <ann.dergachova@gmail.com>
2023-01-25 22:38:03 +0100

    done task 2 lesson 4

--- lesson_4.py
+++ lesson_4.py
@@ -3,3 +3,14 @@
 
 my_string = "john peter brian Morgan Adam Maria bart"
 print(my_string.title())
+
+#there is a list of friends ["John", "Marta", "James", "Amanda", "Marianna"]. print to the console
+#the names of each on a new line, right-aligned using the string method and formatting via f string.
+#Also, above the names, in the first line, display the headings Names where the word names should
+#be in the middle, and the rest of the space is filled with the symbol "*"
+
+my_list = ["John", "Marta", "James", "Amanda", "Marianna"]
+header = 'Names'
+print(header.center(15, '*'))
+for i in range(0, len(my_list)):
+    print(f"{my_list[i] : >10}")
