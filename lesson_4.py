6cd820e8f7da718a734d8646d6aa2c743bbef4cc
DergachovaAnna <ann.dergachova@gmail.com>
2023-01-25 21:52:27 +0100

    done task 1 lesson 4

---
+++ lesson_4.py
@@ -0,0 +1,5 @@
+# there is a space-separated string with the names "john peter brian Morgan Adam Maria bart" convert
+# the string so that each name starts with a capital letter.
+
+my_string = "john peter brian Morgan Adam Maria bart"
+print(my_string.title())
