a0ad2c88c3c83367790616c702b903b841c6c9a7
DergachovaAnna <ann.dergachova@gmail.com>
2023-01-29 22:46:30 +0100

    done task 5 lesson 4

--- lesson_4.py
+++ lesson_4.py
@@ -19,8 +19,8 @@ for name in range(len(my_list)):
     print(f"{my_list[name] : >10}")
 
 
-#there is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
-#Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
+# there is a string " name=Amanda=sssss&age=32&&salary=1500&currency=euro ".
+# Convert this string to a dictionary {name: Amanda, age: 32, salary: 1500, currency: quro}
 
 my_string = " name=Amanda=sssss&age=32&&salary=1500&currency=euro "
 
@@ -42,4 +42,25 @@ for item in my_list:
     my_list_2.append(re.sub(r'(?<!^)(?=[A-Z])', '_', item).lower())
 
 print(my_list_2)
+
+
+# you have a text. break the text(attached file) into sentences. As a result, you should get a list of sentences.
+
+text = "The Hubble.Space.Telescope (often referred to as HST or Hubble) is a space telescope that " \
+       "was launched into low Earth orbit in 1990 and remains in operation.... It was not the first space telescope, " \
+       "but it is one of the largest and most versatile, " \
+       "renowned both as a vital research tool and as a public relations boon for astronomy. " \
+       "The Hubble telescope is named after astronomer Edwin Hubble and is one of NASA's Great Observatories..... " \
+       "The Space Telescope Science Institute (STScI) selects Hubble's targets and processes the resulting data, " \
+       "while the Goddard Space Flight Center (GSFC) controls the spacecraft.A commission headed by Lew Allen, " \
+       "director of the Jet Propulsion Laboratory, was established to determine how the error could have arisen. " \
+       "The Allen Commission found that a reflective null corrector, a testing device used to achieve " \
+       "a properly shaped non-spherical mirror, " \
+       "had been incorrectly assembled—one lens was out of position by 1.3 mm (0.051 in)."
+
+new_text = text.replace('.A', '. A')
+list_of_sentences = new_text.split('.')
+
+for i, text in enumerate(new_text.split('. ')):
+    print(f'[{i+1}]{text}')
 
