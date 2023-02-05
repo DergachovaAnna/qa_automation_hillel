827bd2b7261624f5ff3ca707aaa146f7a13176e2
DergachovaAnna <ann.dergachova@gmail.com>
2023-02-05 21:03:16 +0100

    update task 1 lesson 7

--- lesson_7.py
+++ lesson_7.py
@@ -2,13 +2,12 @@ import sys
 my_file = open('print.txt', 'w')
 
 
-def printing(*args, file=sys.stdout):
-    file.write(str(args))
+def printing(*args, sep=' ', end='\n', file=sys.stdout):
+    file.write(sep.join(map(str, args)))
+    file.write(end)
 
 
 if __name__ == '__main__':
-    printing('John', 'Bill', 'James', file=my_file)
-    printing(123456789)
+    printing('John', 'Bill', 'James', sep='-', file=my_file)
+    printing(123456789, end=' ')
     printing('qwerty')
-
-
