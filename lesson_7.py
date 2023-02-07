af8ba8cb6aff603bb21c268f5df407548b1f6b59
DergachovaAnna <ann.dergachova@gmail.com>
2023-02-07 16:30:41 +0100

    done task 3 lesson 7

--- lesson_7.py
+++ lesson_7.py
@@ -22,23 +22,23 @@ if __name__ == '__main__':
 # Implement your realization of the function filter
 sequence_1 = [1, 3, 5, 4, 3.5, 4.4, 0.1]
 sequence_2 = 'ggdfdgdjjjjhgfghj'
-sequence_3 = (1, 3, 5, 6, 0.9, 4.6)
+sequence_3 = (1, 3, 5, 6, 'f', 'F', 1.8, 7.7)
 
 
-def my_filter(callback, sequence):
+def my_filter(filter_condition, sequence):
     """
-    :param callback: function with filtering condition
+    :param filter_condition: function with filtering condition
     :param sequence: e.g. - list, tuple, string
     :return: filtered list
     """
     result = []
     for item in sequence: # iterate through sequence of items in sequence
-        if callback(item):  # apply filter function
+        if filter_condition(item):  # apply filter function
             result.append(item)
     return result
 
 
-def callback(item):
+def filter_condition (item):
     if item:  # function require filtering statement
     # if (item % 2) == 0:
     # if 'g' in item:
@@ -47,12 +47,36 @@ def callback(item):
         return False
 
 
-print(my_filter(callback, sequence_1))
-print(my_filter(callback, sequence_2))
-print(my_filter(callback, sequence_3))
+print(my_filter(filter_condition, sequence_1))
+print(my_filter(filter_condition, sequence_2))
+print(my_filter(filter_condition, sequence_3))
 
 
+# Implement your own implementation of the function map
+
+def my_map(callback, sequence):
+    """
+    The function updates the given sequence: if the given element has 'str' type - the function will convert it
+    to upper case, if the given element in the sequence has the type int or float - it will multiply it
+    :param callback: function object, which means that you need to pass a function without calling it
+    :param sequence: e.g. - list, tuple, string
+    :return: updated sequence, by applying a transformation function to every item in the original sequence
+    """
+    result = []
+    for item in sequence:
+        result.append(callback(item))
+    return result
 
 
+def callback(item):
+    if type(item) is str:
+        result = item.upper()
+        return result
+    elif type(item) is int or type(item) is float:
+        result = item * item
+        return result
 
 
+print(my_map(callback, sequence_3))
+print(my_map(callback, sequence_2))
+print(my_map(callback, sequence_1))
