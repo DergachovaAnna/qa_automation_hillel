3cf689e1140decc54dd3504d8bc3f49e39e3cc31
DergachovaAnna <ann.dergachova@gmail.com>
2023-02-27 23:34:18 +0100

    have splitted classes to 2 files, renamed methofs to use verbs, fixed other comments

--- lesson_9.py
+++ lesson_9.py
@@ -20,7 +20,7 @@ class AppleCompany:
         self.__location = location
         self.__num_employees = 0
 
-    def location_capacity(self):
+    def calculate_location_capacity(self):
         """This method calculates the number of vacant positions left in the company's location."""
         left_capacity = AppleCompany.__MAX_EMPLOYEES_CAPACITY - self.__num_employees
         if self.__location in AppleCompany.__offices_locations.keys():
@@ -40,11 +40,13 @@ class AppleCompany:
         Args: - new_num_employees (int): The new number of employees.
         Raises: - ValueError: If the new number of employees exceeds the maximum capacity allowed for the company.
         """
-        if new_num_employees <= AppleCompany.__MAX_EMPLOYEES_CAPACITY:
+        if new_num_employees < 0:
+            raise ValueError("Number of employees cannot be negative value")
+        elif new_num_employees > AppleCompany.__MAX_EMPLOYEES_CAPACITY:
+            raise ValueError(f'Exceeded max capacity of {AppleCompany.__MAX_EMPLOYEES_CAPACITY}')
+        else:
             self.__num_employees = new_num_employees
             print(f'Number of employees updated to {new_num_employees}')
-        else:
-            raise ValueError(f'Exceeded max capacity of {AppleCompany.__MAX_EMPLOYEES_CAPACITY}')
 
     @property
     def company_location(self):
@@ -61,7 +63,7 @@ class AppleCompany:
         print(f'Profit in {self.__location} is {profit} USD')
 
     @classmethod
-    def from_country(cls, country: str):
+    def get_office_from_country(cls, country: str):
         """
         Class method to create an instance of AppleCompany based on the country.
         Args: - country (str): The country to get location of the company, taking result by key's value
@@ -90,91 +92,8 @@ class AppleCompany:
         return f'{production_type} cost is {production_cost} USD'
 
 
-# Create a class with a description of the worker. Any worker, employees.
-class Employees:
-
-    def __init__(self, name, age):
-        self.__name = name
-        self.__age = age
-        self.__position = ''
-        self.__min_salary = 2000
-
-    @property
-    def position(self):
-        return self.__position
-
-    @property
-    def min_salary(self):
-        return self.__min_salary
-
-    @property
-    def age(self):
-        return self.__age
-
-    @age.setter
-    def age(self, new_age: int):
-        """
-        Sets the age of the Employee to a new value.
-        param new_age: int
-        """
-        if isinstance(new_age, int):
-            if new_age < self.__age:
-                raise ValueError(f'Sorry, but employee cant get younger. Re-check age value')
-            elif new_age >= self.__age:
-                self.__age = new_age
-
-    def employee_apply_for_job(self, position, min_salary):
-        """
-        This method takes a position, age and a min_salary parameter and checks if the salary offered is greater than
-        or equal to the employee's minimum salary requirement. If it is, the employee is offered the job and their
-        position and minimum salary are updated. Otherwise, employee rejects application.
-        :param position: str
-        :param min_salary: int, float
-        """
-        if 50 >= self.__age >= 20:
-            if min_salary >= self.__min_salary:
-                self.__position = position
-                self.__min_salary = min_salary
-                return f"Application accepted!"
-            else:
-                return f"Sorry, the salary offered for '{position}' position is too low."
-        else:
-            return f'Employees age is not eligible for job application'
-
-    def employee_fire(self):
-        """
-        This method fires the employee by resetting their position and minimum salary to default values.
-        """
-        self.__position = ''
-        self.__min_salary = 2000
-        return f"Employee fired."
-
-    @classmethod
-    def average_age(cls, employee_list):
-        """
-        This method takes a list of employees as input and returns the average age of all employees.
-        param employee_list: list
-        """
-        total_age = 0
-        for employee in employee_list:
-            total_age += employee.age
-            print(employee.age)
-        return total_age / len(employee_list)
-
-
 if __name__ == '__main__':
-    # office_1 = AppleCompany.from_country('USA')
-    # print(office_1.company_location)
-    # print(office_1.location_capacity())
-
-    emp1 = Employees('Bob', 38)
-    # print(emp1.age)
-    # emp1.set_new_age = 55
-    # print(emp1.age)
-    # emp1.set_new_age = 51
-    # print(emp1.age)
-
-    emp2 = Employees('Bill', 30)
-    emp_list = [emp1, emp2]
-    average_age = Employees.average_age(emp_list)
-    print(average_age)
+    office = AppleCompany('USA')
+    print(office.num_employees)
+    office.num_employees = -1
+    print(office.num_employees)
---
+++ lesson_9_2.py
@@ -0,0 +1,90 @@
+# Create a class with a description of the worker. Any worker, employees.
+class Employee:
+
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        self.__position = ''
+        self.__min_salary = 2000
+
+    @property
+    def position(self):
+        return self.__position
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def min_salary(self):
+        return self.__min_salary
+
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, new_age: int):
+        """
+        Sets the age of the Employee to a new value.
+        param new_age: int
+        """
+        max_age = 130
+        if isinstance(new_age, int):
+            if new_age < self.__age:
+                raise ValueError(f'Sorry, but employee cant get younger. Re-check age value')
+            elif self.__age <= new_age <= max_age:
+                self.__age = new_age
+            else:
+                raise ValueError(f'Sorry, but people on Earth do not live that long yet. Re-check age value')
+
+    def apply_for_job(self, position, min_salary):
+        """
+        This method takes a position, age and a min_salary parameter and checks if the salary offered is greater than
+        or equal to the employee's minimum salary requirement. If it is, the employee is offered the job and their
+        position and minimum salary are updated. Otherwise, employee rejects application.
+        :param position: str
+        :param min_salary: int, float
+        """
+        if 50 >= self.__age >= 20:
+            if min_salary >= self.__min_salary:
+                self.__position = position
+                self.__min_salary = min_salary
+                return f"Application accepted!"
+            else:
+                return f"Sorry, the salary offered for '{position}' position is too low."
+        else:
+            return f'Employees age is not eligible for job application'
+
+    def fire(self):
+        """
+        This method fires the employee by resetting their position and minimum salary to default values.
+        """
+        self.__position = ''
+        self.__min_salary = 2000
+        return f"Employee fired."
+
+    @staticmethod
+    def calculate_average_age(employee_list):
+        """
+        This method takes a list of employees as input and returns the average age of all employees.
+        param employee_list: list
+        """
+        total_age = 0
+        for employee in employee_list:
+            total_age += employee.age
+            print(employee.age)
+        return total_age / len(employee_list)
+
+    @classmethod
+    def create_employee(cls, name, age, position, min_salary):
+        employee = cls(name, age)
+        employee.apply_for_job(position, min_salary)
+        return employee
+
+    def __str__(self):
+        return f"Have created {self.name}, with age {self.age}, hired as {self.position}, with salary {self.min_salary}"
+
+
+if __name__ == '__main__':
+    emp1 = Employee('Bob', 38)
