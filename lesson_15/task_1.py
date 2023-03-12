58a5f54d217d6fbdc470e3111bc730e1e38c47b6
DergachovaAnna <ann.dergachova@gmail.com>
2023-03-12 20:18:33 +0100

    hided attributes

--- lesson_15/task_1.py
+++ lesson_15/task_1.py
@@ -4,23 +4,23 @@ import random
 
 
 class CustomIterator:
-    def __init__(self, sequence, start_index, end_index):
-        self.sequence = sequence
-        self.start_index = start_index - 1
-        self.end_index = end_index - 1
-        self.current_index = start_index - 1
+    def __init__(self, sequence: list, start_index: int, end_index: int):
+        self.__sequence = sequence
+        self.__start_index = start_index - 1
+        self.__end_index = end_index - 1
+        self.__current_index = start_index - 1
 
-        if self.start_index < 0 or self.end_index < 0:
+        if self.__start_index < 0 or self.__end_index < 0:
             raise ValueError("Please enter correct index range")
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.current_index >= self.end_index:
+        if self.__current_index >= self.__end_index:
             raise StopIteration
-        sequence_value = self.sequence[self.current_index]
-        self.current_index += 1
+        sequence_value = self.__sequence[self.__current_index]
+        self.__current_index += 1
         return sequence_value
 
 
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     my_list = random.sample(range(1, 30), 10)
     print(my_list)
 
-    iterator = CustomIterator(my_list, 0, 9)
+    iterator = CustomIterator(my_list, 5, 9)
 
     for value in iterator:
         print(value)
